10bb1cf600bdca71b79ba42d423c94f288ec2c16
Gustavo Muniz <cgmds73@gmail.com>
2021-12-29 10:28:51 -0300

    Trying to move classes that maybe (just maybe) seems to be part of the domain

--- src/main/scala/com/eskimi/samplebid/EskimiBid.scala
+++ src/main/scala/com/eskimi/samplebid/EskimiBid.scala
@@ -7,6 +7,7 @@ import akka.http.scaladsl.model.StatusCodes._
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server._
+import com.eskimi.domain.{Banner, BidRequest, BidResponse, Campaign, Impression, Targeting}
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonInclude.Include
 import com.fasterxml.jackson.databind.ObjectMapper
@@ -19,37 +20,39 @@ import scala.collection.mutable.ListBuffer
 import scala.io.StdIn
 import scala.util.Random
 
-case class Banner(id: Int, src: String, width: Int, height: Int)
+//TODO remove if not needed
+//case class Banner(id: Int, src: String, width: Int, height: Int)
 
-@JsonIgnoreProperties(ignoreUnknown = true) case class BidRequest(
-    id: String,
-    imp: Option[List[Impression]],
-    site: Site,
-    user: Option[User],
-    device: Option[Device],
-)
-@JsonIgnoreProperties(ignoreUnknown = true) case class Impression(
-    id: String,
-    wmin: Option[Int],
-    wmax: Option[Int],
-    w: Option[Int],
-    hmin: Option[Int],
-    hmax: Option[Int],
-    h: Option[Int],
-    bidFloor: Option[Double],
-)
-@JsonIgnoreProperties(ignoreUnknown = true) case class Site(id: String, domain: String)
-@JsonIgnoreProperties(ignoreUnknown = true) case class User(id: String, geo: Option[Geo])
-@JsonIgnoreProperties(ignoreUnknown = true) case class Device(id: String, geo: Option[Geo])
-@JsonIgnoreProperties(ignoreUnknown = true) case class Geo(country: Option[String])
-
-case class BidResponse(id: String, bidRequestId: String, price: Double, adid: Option[String], banner: Option[Banner])
-object BidRequest {
-  val mapper = new ObjectMapper() with ScalaObjectMapper
-  mapper.registerModule(DefaultScalaModule)
-  mapper.setSerializationInclusion(Include.NON_EMPTY);
-  def toJson(value: Any) = mapper.writeValueAsString(value)
-}
+//TODO remove if not needed
+//@JsonIgnoreProperties(ignoreUnknown = true) case class BidRequest(
+//    id: String,
+//    imp: Option[List[Impression]],
+//    site: Site,
+//    user: Option[User],
+//    device: Option[Device],
+//)
+//@JsonIgnoreProperties(ignoreUnknown = true) case class Impression(
+//    id: String,
+//    wmin: Option[Int],
+//    wmax: Option[Int],
+//    w: Option[Int],
+//    hmin: Option[Int],
+//    hmax: Option[Int],
+//    h: Option[Int],
+//    bidFloor: Option[Double],
+//)
+//@JsonIgnoreProperties(ignoreUnknown = true) case class Site(id: String, domain: String)
+//@JsonIgnoreProperties(ignoreUnknown = true) case class User(id: String, geo: Option[Geo])
+//@JsonIgnoreProperties(ignoreUnknown = true) case class Device(id: String, geo: Option[Geo])
+//@JsonIgnoreProperties(ignoreUnknown = true) case class Geo(country: Option[String])
+//
+//case class BidResponse(id: String, bidRequestId: String, price: Double, adid: Option[String], banner: Option[Banner])
+//object BidRequest {
+//  val mapper = new ObjectMapper() with ScalaObjectMapper
+//  mapper.registerModule(DefaultScalaModule)
+//  mapper.setSerializationInclusion(Include.NON_EMPTY);
+//  def toJson(value: Any) = mapper.writeValueAsString(value)
+//}
 
 object EskimiBid extends JacksonSupport {
 
